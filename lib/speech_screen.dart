2ecbd6539f75facbd5102b0ad0ff502b2fe16456
User_ZUR <haninfo95@gmail.com>
2023-04-01 10:02:32 +0000

    speech to text final code

--- lib/speech_screen.dart
+++ lib/speech_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/colors.dart';
 import 'package:avatar_glow/avatar_glow.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class SpeechScreen extends StatefulWidget {
   const SpeechScreen({super.key});
@@ -10,24 +11,52 @@ class SpeechScreen extends StatefulWidget {
 }
 
 class _SpeechScreenState extends State<SpeechScreen> {
+  SpeechToText speechToText = SpeechToText();
   var text = 'Hold the button and start speaking';
+  var isListening = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: const AvatarGlow(
+      floatingActionButton: AvatarGlow(
         endRadius: 75.0,
-        animate: true,
+        animate: isListening,
         duration: Duration(milliseconds: 2000),
         glowColor: bgColor,
         repeat: true,
         repeatPauseDuration: Duration(milliseconds: 100),
         showTwoGlows: true,
-        child: CircleAvatar(
-          backgroundColor: bgColor,
-          radius: 35,
-          child: Icon(Icons.mic, color: Colors.white),
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  speechToText.listen(
+                    onResult: (result) {
+                      setState(() {
+                        text = result.recognizedWords;
+                      });
+                    },
+                  );
+                });
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: bgColor,
+            radius: 35,
+            child: Icon(isListening ? Icons.mic : Icons.mic_none,
+                color: Colors.white),
+          ),
         ),
       ),
       appBar: AppBar(
@@ -43,15 +72,23 @@ class _SpeechScreenState extends State<SpeechScreen> {
           ),
         ),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        // color: Colors.red,
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        margin: const EdgeInsets.only(bottom: 150),
-        child: Text(
-          text,
-          style: const TextStyle(
-              fontSize: 24, color: Colors.black54, fontWeight: FontWeight.w600),
+      body: SingleChildScrollView(
+        reverse: true,
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.7,
+          alignment: Alignment.center,
+          //color: Colors.red,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          margin: const EdgeInsets.only(bottom: 150),
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 24,
+                color: isListening ? Colors.black87 : Colors.black54,
+                fontWeight: FontWeight.w600),
+          ),
         ),
       ),
     );
